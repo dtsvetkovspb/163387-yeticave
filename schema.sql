3cca1e24fc68e390dd854f4a24b16802005f6364
dtsvetkovspb <44998241+dtsvetkovspb@users.noreply.github.com>
2019-02-25 17:36:55 +0300

    #9 fixed char -> varchar, password, added NOT NULL

--- schema.sql
+++ schema.sql
@@ -6,27 +6,27 @@ USE yeticave;
 
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name CHAR(128)
+    name VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE lots (
     id INT AUTO_INCREMENT PRIMARY KEY,
     dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    name CHAR(64),
-    description CHAR(128),
-    picture CHAR(128),
-    start_price INT,
-    exp_date DATETIME,
+    name VARCHAR(64) NOT NULL,
+    description VARCHAR(128),
+    picture VARCHAR(128),
+    start_price INT NOT NULL,
+    exp_date DATETIME NOT NULL,
     bet_step INT,
     user_id INT,
-    winner_id CHAR(64),
+    winner_id VARCHAR(64),
     cat_id INT
 );
 
 CREATE TABLE bets (
     id          INT AUTO_INCREMENT PRIMARY KEY,
-    date_add    DATETIME,
-    offer_price INT,
+    date_add    DATETIME NOT NULL,
+    offer_price INT NOT NULL,
     user_id     INT,
     lot_id      INT
 );
@@ -34,11 +34,11 @@ CREATE TABLE bets (
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     registration_date DATETIME,
-    email CHAR(64) UNIQUE,
-    name CHAR(64),
-    password CHAR(128),
-    avatar CHAR(128),
-    contacts CHAR(128),
-    created_lots CHAR(128),
+    email VARCHAR(64) UNIQUE,
+    name VARCHAR(64) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    avatar VARCHAR(128),
+    contacts VARCHAR(128),
+    created_lots VARCHAR(128),
     bets_id INT
 )
