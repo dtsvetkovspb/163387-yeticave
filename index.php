9624d92c880d4ad7baa1aa3b3f91a3df5c5f5e57
dtsvetkovspb <44998241+dtsvetkovspb@users.noreply.github.com>
2019-02-26 17:21:27 +0300

    #11 Connected to SQL from php, categories & lots data now takes from DB

--- index.php
+++ index.php
@@ -1,48 +1,15 @@
 <?php
-require_once('functions.php');
+require_once 'functions.php';
+require_once 'init.php';
+require_once 'mysql_helper.php';
+
+$categories = db_fetch_data($link, 'SELECT name FROM categories');
+$lots = db_fetch_data($link, 'SELECT lots.name, c.name AS cat_name, start_price, picture FROM lots JOIN categories c ON c.id = cat_id');
 
 $isAuth = rand(0, 1);
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
-$lots = [
-    [
-        'Name' => '2014 Rossignol District Snowboard',
-        'Category' => 'Доски и лыжи',
-        'Price' => '10999',
-        'URL' => 'img/lot-1.jpg'
-    ],
-    [
-        'Name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'Category' => 'Доски и лыжи',
-        'Price' => '159999',
-        'URL' => 'img/lot-2.jpg'
-    ],
-    [
-        'Name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'Category' => 'Крепления',
-        'Price' => '8000',
-        'URL' => 'img/lot-3.jpg'
-    ],
-    [
-        'Name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'Category' => 'Ботинки',
-        'Price' => '10999',
-        'URL' => 'img/lot-4.jpg'
-    ],
-    [
-        'Name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'Category' => 'Одежда',
-        'Price' => '7500',
-        'URL' => 'img/lot-5.jpg'
-    ],
-    [
-        'Name' => 'Маска Oakley Canopy',
-        'Category' => 'Разное',
-        'Price' => '5400',
-        'URL' => 'img/lot-6.jpg'
-    ]
-];
 
 $userName = 'Dmitriy'; // укажите здесь ваше имя
+
 $page_content = include_template('main.php', [
     'categories' => $categories,
     'lots' => $lots
--- templates/main.php
+++ templates/main.php
@@ -2,9 +2,9 @@
     <h2 class="promo__title">Нужен стафф для катки?</h2>
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
-        <?php foreach ($categories as $key => $val): ?>
+        <?php foreach ($categories as $cat): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=htmlspecialchars($val);?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$cat['name'];?></a>
             </li>
         <?php endforeach; ?>
     </ul>
@@ -17,15 +17,15 @@
         <?php foreach ($lots as $key => $val): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=htmlspecialchars($val['URL']);?>" width="350" height="260" alt="<?=$val['Name'];?>">
+                    <img src="<?=$val['picture'];?>" width="350" height="260" alt="<?=$val['name'];?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=htmlspecialchars($val['Category']);?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($val['Name']);?></a></h3>
+                    <span class="lot__category"><?=$val['cat_name'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['name'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <!--<span class="lot__amount"><?/*=$val['Price'];*/?></span>-->
-                            <span class="lot__cost"><?=htmlspecialchars(formattedNum($val['Price']));?></span>
+                            <span class="lot__cost"><?=formattedNum($val['start_price']);?></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=timeToNextDay();?>
