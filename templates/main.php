c15286fcbcd4fe32741b571098e2ec665228cd69
dtsvetkovspb <44998241+dtsvetkovspb@users.noreply.github.com>
2019-02-27 17:05:01 +0300

    #12 Added new scripts and templates, lot page is now takes data from DB

--- templates/main.php
+++ templates/main.php
@@ -21,7 +21,7 @@
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?=$val['cat_name'];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['name'];?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="lot.php?id=<?=$val['id']?>"><?=$val['name'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <!--<span class="lot__amount"><?/*=$val['Price'];*/?></span>-->
